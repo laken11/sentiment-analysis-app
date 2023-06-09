bb449d01de61e5b037ba3de45671e0fe6960c64a
laken11 <olalekanolanite@gmail.com>
2023-06-09 22:13:56 +0000

    updates

--- TETFUNDHR/settings.py
+++ TETFUNDHR/settings.py
@@ -11,9 +11,20 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from pathlib import Path
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+# Create .env file path.
+dotenv_path = join(dirname(__file__), '.env')
+
+print(f"dotenv_path = {dotenv_path}") 
+# Load file from the path.
+load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
